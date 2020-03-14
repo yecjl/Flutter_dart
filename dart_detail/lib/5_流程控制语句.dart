ca3f3df2eef9e4e0ac081bb88e2f2f01149ef5ea
danke <danke@2dfire.com>
2020-03-14 16:57:43 +0800

    [MOD] 5_流程控制语句

---
+++ dart_detail/lib/5_流程控制语句.dart
@@ -0,0 +1,14 @@
+main() {
+  // if else
+  // for, forEach, for-in
+  // while , do-while
+  // break , continue
+  // switch case
+
+
+  // 【新增】for-in
+  var list = [1,2,3];
+  for (var elem in list) {
+    print('elem = $elem');
+  }
+}
