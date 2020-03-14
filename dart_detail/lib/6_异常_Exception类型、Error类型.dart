60e4e8ae414a5c6b87fc83053578bf8ee95fc053
danke <danke@2dfire.com>
2020-03-14 17:08:26 +0800

    [MOD] 6_异常_Exception类型、Error类型

---
+++ dart_detail/lib/6_异常_Exception类型、Error类型.dart
@@ -0,0 +1,32 @@
+main() {
+  /**
+   *  1、异常-Exception类型
+      DeferredLoadException	延迟加载异常
+      FormatException	格式异常
+      IntegerDivisionByZeroException	整数除零异常
+      IOException	IO异常
+      IsolateSpawnException	隔离产生异常
+      TimeoutException	超时异常
+   */
+
+  /**
+   *  2、异常-Error类型
+      AbstractClassInstantiationError	抽象类实例化错误
+      ArgumentError	参数错误
+      AssertionError	断言错误
+      AsyncError	异步错误
+      CastError	Cast 错误
+      ConcurrentModificationError	并发修改错误
+      CyclicInitializationError	周期初始错误
+      FallThroughError	Fall Through 错误
+      JsonUnsupportedObjectError	json 不支持错误
+      NoSuchMethodError	没有这个方法错误
+      NullThrownError	Null 错误
+      OutOfMemoryError	内存溢出错误
+      RemoteError	远程错误
+      StackOverflowError	堆栈溢出错误
+      StateError	状态错误
+      UnimplementedError	未实现的错误
+
+   */
+}
