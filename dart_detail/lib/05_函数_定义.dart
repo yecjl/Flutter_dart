2de066a87ba77b2b8b6f91292d29323b0e3f0224
danke <danke@2dfire.com>
2020-03-14 09:48:49 +0800

    [MOD] 05_函数_定义

--- dart_detail/lib/05_函数_定义.dart
+++ dart_detail/lib/05_函数_定义.dart
@@ -1,17 +1,31 @@
 main() {
-  // 可在函数内定义
-  // 定义函数时可省略类型
-  // 支持缩写语法
-
-  // 1、返回类型可省略
   print(test()); // null 一切皆对象
   print("add: " + add(1, 2).toString());
 }
 
+/// 定义函数时可省略类型
 test() {
   // 默认会有一行 return null;
 }
 
+/// 最好加上返回值
 int add(int a, int b) {
   return a + b;
+}
+
+/// 支持缩写语法 =>
+int multiply(int a, int b) => a * b;
+
+/// 可在函数内定义
+method () {
+  // 可以无限嵌套
+  int add(int a, int b) {
+    int add(int a, int b) {
+      int add(int a, int b) {
+        return a + b;
+      }
+      return a + b;
+    }
+    return a + b;
+  }
 }
