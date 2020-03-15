dd2d506c5dcbede083a7e1cb4888b016790da86b
danke <danke@2dfire.com>
2020-03-15 13:40:14 +0800

    [MOD] 7.2_ 类_Setter和Getter

---
+++ dart_detail/lib/7.2_ 类_Setter和Getter.dart
@@ -0,0 +1,24 @@
+main() {
+  var rectangle = Rectangle(0,0,1,1);
+  print([rectangle.top, rectangle.bottom, rectangle.left, rectangle.right, rectangle.width, rectangle.height]);
+  rectangle.right = 20;
+  rectangle.bottom = 30;
+  print([rectangle.top, rectangle.bottom, rectangle.left, rectangle.right, rectangle.width, rectangle.height]);
+}
+
+class Rectangle {
+  num top; // 默认有get,set  num get top => this.top
+  num left;
+  num width;
+  num height;
+
+  Rectangle(this.top, this.left, this. width, this.height);
+
+  num get bottom => top + height;
+
+  set bottom(num value) => top = value - height;
+
+  num get right => left + width;
+
+  set right(num value) => left = value - width;
+}
